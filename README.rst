2284f3eca6e203afff237d194f33143830378f1b
Jason K. Moore <moorepants@gmail.com>
2025-03-21 05:13:17 +0100

    Added two starter scripts from gait2d and opty.

--- README.rst
+++ README.rst
@@ -1,1 +1,33 @@
-Fill me in!
+Setup
+=====
+
+Clone the two repositories where the developement is done::
+
+   git clone git@github.com:mechmotum/gait-closed-loop-id.git
+   git clone git@github.com:csu-hmc/gait2d.git
+
+Install the conda environment for the gait closed loop id project::
+
+   cd gait-closed-loop-id
+   conda env create -f gait-closed-loop-id-env.yml
+   conda activate gait-closed-loop-id
+
+Install an editable development version of gait2d::
+
+   cd ../gait2d
+   python -m pip install --no-dependencies --no-build-isolation --editable  .
+
+Change back into the project repository::
+
+   cd ../gait-closed-loop-id
+
+Run
+===
+
+Run the code that evaluates the differential equations::
+
+   python src/evaluate.py
+
+Solve an optimal control problem::
+
+   python src/solve.py
---
+++ src/evaluate.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This example simply simulates and visualizes the uncontrolled motion and
+the model "falls down"."""
+
+import os
+
+import numpy as np
+from scipy.integrate import odeint
+from pydy.codegen.ode_function_generators import generate_ode_function
+from pydy.viz import Scene
+
+from pygait2d import derive, simulate
+
+(mass_matrix, forcing_vector, kane, constants, coordinates, speeds, specified,
+ visualization_frames, ground, origin, segments) = \
+    derive.derive_equations_of_motion()
+
+constant_values = simulate.load_constants(
+    constants, os.path.join(os.path.dirname(__file__), '..',
+                            'data/example_constants.yml'))
+
+rhs = generate_ode_function(
+    forcing_vector,
+    coordinates,
+    speeds,
+    constants=list(constant_values.keys()),
+    mass_matrix=mass_matrix,
+    specifieds=specified,
+    generator='cython',
+    constants_arg_type='array',
+    specifieds_arg_type='array',
+)
+
+
+args = (np.zeros(len(specified)), np.array(list(constant_values.values())))
+
+time_vector = np.linspace(0.0, 10.0, num=1000)
+initial_conditions = np.zeros(len(coordinates + speeds))
+initial_conditions[1] = 1.0  # set hip above ground
+initial_conditions[3] = np.deg2rad(5.0)  # right hip angle
+initial_conditions[6] = -np.deg2rad(5.0)  # left hip angle
+trajectories = odeint(rhs, initial_conditions, time_vector, args=args)
+
+scene = Scene(ground, origin, *visualization_frames)
+scene.states_symbols = coordinates + speeds
+scene.constants = constant_values
+scene.states_trajectories = trajectories
+scene.times = time_vector
