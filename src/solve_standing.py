7a0f54bd7f7d2f92ae565f8117e52ff30df84644
Jason K. Moore <moorepants@gmail.com>
2026-02-22 08:24:03 +0100

    Simplify solve_standing.py.

--- src/solve_standing.py
+++ src/solve_standing.py
@@ -12,12 +12,9 @@ from pygait2d.derive import derive_equations_of_motion
 from pygait2d.segment import time_symbol
 import numpy as np
 
-# %%
-# some settings
 num_nodes = 2
 h = 1.0  # time step does not matter, since it is a static equilibrium
 
-# %%
 # Derive the equations of motion
 symbolics = derive_equations_of_motion(prevent_ground_penetration=False,
                                        treadmill=True, hand_of_god=False)
@@ -26,15 +23,6 @@ eom = symbolics.equations_of_motion
 states = symbolics.states
 num_states = len(states)
 
-# %%
-# The generalized coordinates are the hip lateral position :math:`q_{ax}` and
-# veritcal position :math:`q_{ay}`, the trunk angle with respect to vertical
-# :math:`q_a` and the relative joint angles:
-#
-# - right: hip (b), knee (c), ankle (d)
-# - left: hip (e), knee (f), ankle (g)
-#
-# Each joint has a joint torque acting between the adjacent bodies.
 qax, qay, qa, qb, qc, qd, qe, qf, qg = symbolics.coordinates
 uax, uay, ua, ub, uc, ud, ue, uf, ug = symbolics.speeds
 Tb, Tc, Td, Te, Tf, Tg, v = symbolics.specifieds
@@ -58,36 +46,28 @@ bounds = {
     qay: (0.5, 1.5),
 }
 # trunk, hip, knee, ankle angles should all be small
-bounds.update({k: (-0.1, 0.1)
-               for k in [qa, qb, qc, qd, qe, qf, qg]})
+bounds.update({k: (-0.1, 0.1) for k in [qa, qb, qc, qd, qe, qf, qg]})
 # horizontal position and all speeds should be zero
 bounds.update({k: (0.0, 0.0)
                for k in [qax, uax, uay, ua, ub, uc, ud, ue, uf, ug]})
 # joint torques should be small
-bounds.update({k: (-10.0, 10.0)
-               for k in [Tb, Tc, Td, Te, Tf, Tg]})
+bounds.update({k: (-10.0, 10.0) for k in symbolics.joint_torques})
 
-# %%
-# No instance constraints are needed
-instance_constraints = None
 
-# %%
 # Objective is integral of sum of squared torques
-num_angles = 6
-torque_indices = num_states*num_nodes + np.arange(0, num_angles*num_nodes)
+def obj(p, free):
+    """Return sum of squares of joint torques."""
+    tor_vals = p.extract_values(free, *symbolics.joint_torques)
+    return np.sum(tor_vals**2)
 
 
-def obj(free):
-    return np.sum(free[torque_indices]**2)
-
-
-def obj_grad(free):
+def obj_grad(p, free):
     grad = np.zeros_like(free)
-    grad[torque_indices] = 2.0*free[torque_indices]
+    tor_vals = p.extract_values(free, *symbolics.joint_torques)
+    p.fill_free(grad, 2.0*tor_vals, *symbolics.joint_torques)
     return grad
 
 
-# %%
 # Create an optimization problem and solve it.
 print("Creating the opty problem.")
 prob = Problem(
@@ -99,13 +79,10 @@ prob = Problem(
     h,
     known_parameter_map=par_map,
     known_trajectory_map=traj_map,
-    instance_constraints=instance_constraints,
     bounds=bounds,
     time_symbol=time_symbol,
-    tmp_dir='gait_codegen',  # enables binary caching
 )
 
-# %%
 # Find the optimal solution and save it if it converges.
 #
 initial_guess = (0.5*(prob.lower_bound + prob.upper_bound) +
